c2912289f673e202536e380a66b40bd81ac1391c
Purnaman Rai <purnamaanrai7@gmail.com>
2025-08-15 06:35:51 +0545

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Purnaman Rai (College) on 15/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
