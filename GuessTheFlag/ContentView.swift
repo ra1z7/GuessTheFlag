edb6b4b3b620c9a74a72ef8ae4946612e69c6206
Purnaman Rai <purnamaanrai7@gmail.com>
2025-08-15 07:41:29 +0545

    overview

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -8,14 +8,46 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showingAlert = false
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Button("Bordered Button") { }
+                .buttonStyle(.bordered)
+            
+            Button("Bordered Button (Destructive)", role: .destructive) { }
+                .buttonStyle(.bordered)
+            
+            Button("Bordered Prominent Button") { }
+                .buttonStyle(.borderedProminent)
+            
+            Button("Bordered Prominent Button (Destructive)", role: .destructive) { }
+                .buttonStyle(.borderedProminent)
+            
+            Button("Custom Background Color") { }
+                .buttonStyle(.bordered)
+                .clipShape(.capsule)
+                .tint(.orange)
+            
+            Button {
+                showingAlert = true
+            } label: {
+                Label("Delete", systemImage: "trash")
+                    .padding(10)
+                    .background(LinearGradient(colors: [.red, .green, .blue], startPoint: .topLeading, endPoint: .bottomTrailing))
+                    .foregroundStyle(.white)
+                    .clipShape(.capsule)
+            }
+            .alert("Alert Title Here", isPresented: $showingAlert) {
+                Button("Cancel", role: .cancel) { }
+                Button("Delete", role: .destructive) { }
+            } message: {
+                Text("Message Here")
+            }
+
+            
+            
         }
-        .padding()
     }
 }
 
