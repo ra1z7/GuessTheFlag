74a7e1c522884a166fadb870644bc51e36f86af7
Purnaman Rai <purnamaanrai7@gmail.com>
2025-08-15 13:56:12 +0545

    project complete

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -8,46 +8,79 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var showingAlert = false
+    @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Spain", "UK", "Ukraine", "US"].shuffled()
+    @State private var correctAnswer = Int.random(in: 0...2)
+    
+    @State private var userScore = 0
+    @State private var showingScoreAlert = false
+    @State private var scoreAlertTitle = ""
     
     var body: some View {
-        VStack {
-            Button("Bordered Button") { }
-                .buttonStyle(.bordered)
+        ZStack {
+            LinearGradient(colors: [.blue, .black], startPoint: .top, endPoint: .bottom)
+                .ignoresSafeArea()
             
-            Button("Bordered Button (Destructive)", role: .destructive) { }
-                .buttonStyle(.bordered)
-            
-            Button("Bordered Prominent Button") { }
-                .buttonStyle(.borderedProminent)
-            
-            Button("Bordered Prominent Button (Destructive)", role: .destructive) { }
-                .buttonStyle(.borderedProminent)
-            
-            Button("Custom Background Color") { }
-                .buttonStyle(.bordered)
-                .clipShape(.capsule)
-                .tint(.orange)
-            
-            Button {
-                showingAlert = true
-            } label: {
-                Label("Delete", systemImage: "trash")
-                    .padding(10)
-                    .background(LinearGradient(colors: [.red, .green, .blue], startPoint: .topLeading, endPoint: .bottomTrailing))
+            VStack {
+                Text("GUESS THE FLAG")
                     .foregroundStyle(.white)
-                    .clipShape(.capsule)
+                    .font(.largeTitle.bold())
+                
+                Spacer()
+                
+                VStack(spacing: 30) {
+                    VStack {
+                        Text("Tap the flag of")
+                            .font(.subheadline.weight(.heavy))
+                            .foregroundStyle(.secondary)
+                        Text(countries[correctAnswer])
+                            .font(.largeTitle.bold())
+                            .foregroundStyle(.white)
+                    }
+                    
+                    ForEach(0..<3) { flagNumber in
+                        Button {
+                            checkAnswer(flagNumber)
+                        } label: {
+                            Image(countries[flagNumber])
+                                .clipShape(RoundedRectangle(cornerRadius: 15))
+                                .shadow(radius: 5)
+                        }
+                    }
+                }
+                .frame(maxWidth: .infinity)
+                .padding(.vertical, 20)
+                .background(.thinMaterial)
+                .clipShape(.rect(cornerRadius: 15))
+                
+                Spacer()
+                
+                Text("SCORE: \(userScore)")
+                    .foregroundStyle(.white)
+                    .font(.title.bold())
             }
-            .alert("Alert Title Here", isPresented: $showingAlert) {
-                Button("Cancel", role: .cancel) { }
-                Button("Delete", role: .destructive) { }
-            } message: {
-                Text("Message Here")
-            }
-
-            
-            
+            .padding()
         }
+        .alert(scoreAlertTitle, isPresented: $showingScoreAlert) {
+            Button("Continue", action: askNextQuestion)
+        } message: {
+            Text("Your Score: \(userScore)")
+        }
+    }
+    
+    func checkAnswer(_ tappedFlag: Int) {
+        if tappedFlag == correctAnswer {
+            userScore += 1
+            scoreAlertTitle = "Correct!"
+        } else {
+            scoreAlertTitle = "Oops, wrong!"
+        }
+        
+        showingScoreAlert = true
+    }
+    
+    func askNextQuestion() {
+        countries.shuffle()
+        correctAnswer = Int.random(in: 1...2)
     }
 }
 
