c5b96cfe1554d2bc010f974b6bc14df03be7ee5a
Purnaman Rai <purnamaanrai7@gmail.com>
2025-08-27 15:18:09 +0545

    Complete Day 34 challenge

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -31,6 +31,12 @@ struct ContentView: View {
     @State private var scoreAlertMessage = ""
     @State private var askedQuestionsCount = 0
     
+    @State private var spinAmount = 0.0
+    @State private var flagTapped = 0
+    @State private var flagOpacity = 1.0
+    @State private var scaleAmount = 1.0
+    @State private var tappedFlagScale = 1.0
+    
     var body: some View {
         ZStack {
             LinearGradient(colors: [.blue, .black], startPoint: .top, endPoint: .bottom)
@@ -56,8 +62,17 @@ struct ContentView: View {
                     ForEach(0..<3) { flagNumber in
                         Button {
                             checkAnswer(flagNumber)
+                            
+                            withAnimation {
+                                spinAmount += 360.0
+                                flagOpacity = 0.25
+                                scaleAmount = 0
+                            }
                         } label: {
                             FlagImage(for: countries[flagNumber])
+                                .rotation3DEffect(.degrees(spinAmount), axis: (x: 0, y: (flagTapped == flagNumber ? 1 : 0), z: 0))
+                                .opacity(flagTapped == flagNumber ? 1 : flagOpacity)
+                                .scaleEffect(flagTapped == flagNumber ? tappedFlagScale : scaleAmount)
                         }
                     }
                 }
@@ -89,6 +104,7 @@ struct ContentView: View {
     
     func checkAnswer(_ tappedFlag: Int) {
         askedQuestionsCount += 1
+        flagTapped = tappedFlag
         
         if tappedFlag == correctAnswer {
             userScore += 1
@@ -105,6 +121,13 @@ struct ContentView: View {
     func askNextQuestion() {
         countries.shuffle()
         correctAnswer = Int.random(in: 1...2)
+        flagOpacity = 1.0
+        tappedFlagScale = 0.0
+        
+        withAnimation {
+            scaleAmount = 1.0
+            tappedFlagScale = 1.0
+        }
     }
     
     func startNewGame() {
